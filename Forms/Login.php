33fab03a33d239da9e5822aaa1b6019e8b777425
Henry Reeves <hreeves@islander.tamucc.edu>
2018-10-11 20:33:48 -0500

    Adding new files

--- Forms/Login.php
+++ Forms/Login.php
@@ -7,22 +7,48 @@
  */?>
 <!DOCTYPE html>
 <html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <head>
     <link rel="stylesheet" type="text/css" href="../../SoftwareEngineeringProject/Master/CSS/master.css">
+    <style>
+        body
+        {
+            font-family: Arial, Helvetica, sans-serif;
+        }
+
+        form
+        {
+            display: flex;
+            justify-content: center;
+        }
+
+        form > div
+        {
+            text-align: center;
+            font-size: 18px;
+        }
+    </style>
 </head>
 <body>
 <div class="flex-container">
-    <div id="divLogin">
-        <div id="divTitle">
-            <h1>Employee Management System</h1>
-        </div>
-        <div id="divEmployeeNumber">
-            <input id="txtEmployeeNumber" type="text" placeholder="Enter Employee Number" required>
-        </div>
-        <div id="divPassword">
-            <input id="txtPassword" type="password" placeholder="Enter Password" required>
-        </div>
+    <div id="divTitle">
+        <h1>Login</h1>
     </div>
+    <form id="formLogin" action="../../SoftwareEngineeringProject/Master/Server_Scripts/LoginManager.php" method="post" accept-charset="UTF-8">
+        <div>
+            <div id="divEmployeeNumber">
+                <label for="enumber"><b>Employee Number: </b></label><br>
+                <input type="text" placeholder="Enter Employee Number" name="enumber" required>
+            </div>
+            <div id="divPassword" style="padding-bottom: 10px;">
+                <label for="psw"><b>Password: </b></label><br>
+                <input type="password" placeholder="Enter Password" name="psw" required>
+            </div>
+            <div id="divSubmitBtn">
+                <button type="submit">Login</button>
+            </div>
+        </div>
+    </form>
 </div>
 </body>
 </html>
---
+++ Master/Server_Scripts/LoginManager.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Court
+ * Date: 10/11/2018
+ * Time: 8:00 PM
+ */
+if(isset($_POST["enumber"]) && isset($_POST["psw"]))
+{
+    
+}
