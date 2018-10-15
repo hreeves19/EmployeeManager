571aca3c4342c48bdb23caa474033d3c798113ba
Ruben <rortiz18@islander.tamucc.edu>
2018-10-14 21:21:57 -0500

    Added sign up page

--- Classes/DBHelper.php
+++ Classes/DBHelper.php
@@ -128,4 +128,6 @@ class DBHelper
         }
         $conn->close();
     }
+
+
 }
--- Master/Server_Scripts/SignUpManager.php
+++ Master/Server_Scripts/SignUpManager.php
@@ -5,3 +5,5 @@
  * Date: 10/14/2018
  * Time: 3:52 PM
  */
+
+require_once("C:/xampp/htdocs/EmployeeManager/Classes/DBHelper.php");
