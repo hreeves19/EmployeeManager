ff2e1b093f0dc7e334ed9c36d2e1881e63b9f725
hreeves19 <hreeves@islander.tamucc.edu>
2018-10-14 23:10:42 -0500

    Updating existing code, adding bootstrap template files

--- Forms/Home.php
+++ Forms/Home.php
@@ -11,21 +11,26 @@ $manager = $_SESSION['manager_id'];
 ?>
 <!DOCTYPE html>
 <html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Home</title>
+<head>
+    <link rel="stylesheet" type="text/css" href="../../EmployeeManager/Master/CSS/master.css">
+</head>
 <body>
+<div class="flex-container">
+    <div>
+        <?php
+        if($manager > 0)
+        {
+            echo "<h1>Welcome Manager $firstname!</h1>";
+        }
 
-<?php
-echo "<br>Manager: $manager";
-if($manager > 0)
-{
-    echo "<h1>Welcome Manager $firstname!</h1>";
-}
-
-else
-{
-    echo "<h1>Welcome $firstname!</h1>";
-}
-?>
-
+        else
+        {
+            echo "<h1>Welcome $firstname!</h1>";
+        }
+        ?>
+    </div>
+</div>
 </body>
 </html
